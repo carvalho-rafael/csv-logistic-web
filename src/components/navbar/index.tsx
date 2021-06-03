5e1f7142b5a1d88d3faea40ad982b9fd32bbe49c
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-06-03 14:04:04 -0300

    create navbar component

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@material-ui/core";
+
+export default function Navbar() {
+  return (
+    <Container>
+      <header>
+        <h1>⚙️ CSV - Logistic</h1>
+      </header>
+    </Container>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@material-ui/core'
 import Head from 'next/head'
+import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Head>
         <title>CSV- Logistic</title>
       </Head>
+      <Navbar />
       <Container>
         <main>
           <p>Hello</p>
