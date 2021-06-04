af66b06e5347e2986025ee26384352edb3fe6ece
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-06-04 01:32:05 -0300

    fetch operators and clients on start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,59 @@
-import { Container } from '@material-ui/core'
+import axios from 'axios'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
-import Navbar from '../components/navbar'
 
-export default function Home() {
+import { ClientsProvider, OperatorClients } from '../contexts/clientsContext'
+import { Operator, OperatorsProvider } from '../contexts/operatorsContext'
+
+import OperatorClientsList from '../components/clientsList'
+import ClientsRegisterForm from '../components/clientsRegisterForm'
+import Navbar from '../components/navbar'
+import OperatorsList from '../components/operatorsList'
+
+import { ClientsContainer, HomeContainer } from '../styles/pages/index'
+
+type HomeProps = {
+  operators: Operator[],
+  clients: OperatorClients[]
+}
+
+export default function Home({ operators, clients }: HomeProps) {
   return (
     <>
       <Head>
         <title>CSV- Logistic</title>
       </Head>
       <Navbar />
-      <Container>
-        <main>
-          <p>Hello</p>
-        </main>
-      </Container>
+      <OperatorsProvider>
+        <ClientsProvider>
+          <HomeContainer>
+            <main>
+              <ClientsContainer>
+                <ClientsRegisterForm />
+                <OperatorClientsList clients={clients} />
+              </ClientsContainer>
+              <OperatorsList operators={operators} />
+            </main>
+          </HomeContainer>
+        </ClientsProvider>
+      </OperatorsProvider>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const operators = await axios
+    .get(`${process.env.NEXT_PUBLIC_API}operators`)
+    .then(response => response)
+
+  const clients = await axios
+    .get(`${process.env.NEXT_PUBLIC_API}clients`)
+    .then(response => response)
+
+  return {
+    props: {
+      operators,
+      clients
+    }
+  }
 }
---
+++ src/styles/pages/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { Container } from '@material-ui/core'
+
+export const HomeContainer = styled(Container)`
+  main {
+    display: flex;
+  }
+`
+
+export const ClientsContainer = styled(Container)`
+  flex: 1;
+`
